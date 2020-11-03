88b4f61405eae0d8203d474d2041bc76f753dead
alex (is a badger, an IRL badger) <73854333+alexisabadger@users.noreply.github.com>
2020-11-03 11:26:05 -0500

    Add files via upload
    This Python function makes a list of years between X and Y (e.g. 1950 and 1990).
    It prints the list and tells you the length of it.
    
    Sample uses: making the key values in a dictionary, other uses.

---
+++ ch7_make_key_list.py
@@ -0,0 +1,11 @@
+# souce: AlexIsABadger
+
+def key_list_maker():
+	# set range to (start year, end year + 1), so 1950, 1990+1
+    key_list = list(range(1950,1990+1))
+    length = len(key_list)
+    print('The length of this list is:', length)
+    print(key_list)			# comment/uncomment to test
+
+key_list_maker()
+
